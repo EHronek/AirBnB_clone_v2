fa7509102d825bfe139740a49e95305c63035f35
EHronek <emmanuelhronek@gmail.com>
2025-01-23 19:33:45 +0300

    setup script

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -4,12 +4,6 @@
 sudo apt-get -y update ; sudo apt -y update
 sudo apt-get -y install nginx
 
-nginx_pid=$(pgrep nginx)
-
-if [ -z "$nginx_pid" ]; then
-    /etc/init.d/nginx start
-fi
-
 sudo ufw allow "Nginx HTTP"
 
 sudo mkdir -p /data/
@@ -26,16 +20,22 @@ sudo echo "<html>
   </body>
 </html>" | sudo tee /data/web_static/releases/test/index.html
 
+if [ -L /data/web_static/current ]; then
+	sudo rm /data/web_static/current
+fi
 sudo ln -s -f /data/web_static/releases/test/ /data/web_static/current
 
 sudo chown -R ubuntu:ubuntu /data/
 
 #sudo sed -i '/listen 80;/a location /hbnb_static { alias /data/web_static/current/;}' /etc/nginx/sites-enabled/default
-if ! grep -q "location /hbnb_static/" "/etc/ngonx/sites-available/default"; then
-	sudo sed -i '/server_name _;/a \\
-	location /hbnb_static/ {
-	    alias /data/web_static/current/;
-	}' "/etc/nginx/sites-available/default"
+CONFIG_FILE="/etc/nginx/sites-available/default"
+if ! grep -q "location /hbnb_static/" "$CONFIG_FILE"; then
+    sudo sed -i '/server_name _;/a \
+    location /hbnb_static/ { \
+        alias /data/web_static/current/; \
+        index index.html; \
+    }' "$CONFIG_FILE"
 fi
 
 sudo service nginx restart
+
