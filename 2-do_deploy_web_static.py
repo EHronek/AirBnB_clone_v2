0bf5a52579e362a7f1fc693c0213ac060851e70f
EHronek <emmanuelhronek@gmail.com>
2025-02-04 13:26:48 +0300

    2 deploy

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -8,7 +8,8 @@ Return:- false if the file at the path archive_path doesn't exit
 from fabric.api import put, run, env
 import os
 env.hosts = ['52.3.247.198', '54.86.157.210']
-env.key_filename = "~/.ssh/id_rsa"
+# env.key_filename = "~/.ssh/id_rsa"
+
 
 def do_deploy(archive_path):
     """distributes an archive to the web servers"""
@@ -17,15 +18,15 @@ def do_deploy(archive_path):
     try:
         archive_file = archive_path.split("/")[-1]
         arc_no_ext = archive_file.split(".")[0]
-        dec_path = "/data/web_static/releases/"
+        path = "/data/web_static/releases/"
         put(archive_path, '/tmp/')
-        run('mkdir -p {}{}/'.format(dec_path, arc_no_ext))
-        run('tar -xzf /tmp/{} -C {}{}/'.format(archive_file, dec_path, arc_no_ext))
+        run('mkdir -p {}{}/'.format(path, arc_no_ext))
+        run('tar -xzf /tmp/{} -C {}{}/'.format(archive_file, path, arc_no_ext))
         run('rm /tmp/{}'.format(archive_file))
-        run('mv {0}{1}/web_static/* {0}{1}/'.format(dec_path, arc_no_ext))
-        run('rm -rf {}{}/web_static'.format(dec_path, arc_no_ext))
+        run('mv {0}{1}/web_static/* {0}{1}/'.format(path, arc_no_ext))
+        run('rm -rf {}{}/web_static'.format(path, arc_no_ext))
         run('rm -rf /data/web_static/current')
-        run('ln -s {}{}/ /data/web_static/current'.format(dec_path, arc_no_ext))
+        run('ln -s {}{}/ /data/web_static/current'.format(path, arc_no_ext))
         return True
-    except:
+    except Exception as e:
         return False
