7b6d21036c8b324019ef477e0d1179f59629bc91
EHronek <emmanuelhronek@gmail.com>
2025-02-04 13:38:13 +0300

    3 deploy

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-Fabric script (based on the file 2-do_deploy_web_static.py) 
+Fabric script (based on the file 2-do_deploy_web_static.py)
 that creates and distributes an archive to your web servers,
 using the function deploy"""
 from fabric.api import *
@@ -8,7 +8,7 @@ import os
 from datetime import datetime
 
 env.hosts = ['52.3.247.198', '54.86.157.210']
-env.key_filename = "~/.ssh/id_rsa"
+
 
 def do_pack():
     """Distributes archive to my web server"""
@@ -26,6 +26,7 @@ def do_pack():
     else:
         return None
 
+
 def do_deploy(archive_path):
     """distributes an archive to the web servers"""
     if os.path.exists(archive_path) is False:
@@ -33,18 +34,19 @@ def do_deploy(archive_path):
     try:
         archive_file = archive_path.split("/")[-1]
         arc_no_ext = archive_file.split(".")[0]
-        dec_path = "/data/web_static/releases/"
+        path = "/data/web_static/releases/"
         put(archive_path, '/tmp/')
-        run('mkdir -p {}{}/'.format(dec_path, arc_no_ext))
-        run('tar -xzf /tmp/{} -C {}{}/'.format(archive_file, dec_path, arc_no_ext))
+        run('mkdir -p {}{}/'.format(path, arc_no_ext))
+        run('tar -xzf /tmp/{} -C {}{}/'.format(archive_file, path, arc_no_ext))
         run('rm /tmp/{}'.format(archive_file))
-        run('mv {0}{1}/web_static/* {0}{1}/'.format(dec_path, arc_no_ext))
-        run('rm -rf {}{}/web_static'.format(dec_path, arc_no_ext))
+        run('mv {0}{1}/web_static/* {0}{1}/'.format(path, arc_no_ext))
+        run('rm -rf {}{}/web_static'.format(path, arc_no_ext))
         run('rm -rf /data/web_static/current')
-        run('ln -s {}{}/ /data/web_static/current'.format(dec_path, arc_no_ext))
+        run('ln -s {}{}/ /data/web_static/current'.format(path, arc_no_ext))
         return True
-    except:
+    except Exception as e:
         return False
+
 
 def deploy():
     """creates and distributes an archive to the web servers"""
