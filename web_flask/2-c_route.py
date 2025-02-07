c7ad2a70e3525b62b2cafa12996c9050ee2e3ec8
EHronek <emmanuelhronek@gmail.com>
2025-02-07 13:16:21 +0300

    HBNB

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -4,6 +4,7 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route("/", strict_slashes=False)
 def hello_hbnb():
     """Handles request for root"""
@@ -20,6 +21,7 @@ def hbnb():
 def cisfun(text):
     """Displays 'c' followed by value of text """
     return 'C ' + text.replace('_', ' ')
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port='5000')
