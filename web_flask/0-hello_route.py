81e8450aba15e41c72c397d7fcb3bf8e1d0cf649
EHronek <emmanuelhronek@gmail.com>
2025-02-07 12:37:54 +0300

    Hello Flask fix

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 """
 Starts a Flask web application
 """
@@ -6,10 +6,12 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route("/", strict_slashes=False)
 def hello_hbnb():
     """Displays hello HBNB! when root url is hit"""
     return "Hello HBNB!"
+
 
 if __name__ == "__main__":
     # Listen in all available network address in my local machine
